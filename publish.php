5ead1d20b06222c79ffcd262fbb66ef968e37b0b
sokmeng <vandy.nem@rightsledger.global>
2020-07-23 09:16:31 +0700

    connect activemq, mqtt with php

--- publish.php
+++ publish.php
@@ -1,36 +1,18 @@
 <?php
-require('phpMQTT.php');
-$server = 'localhost';     // change if necessary
-$port = 1883;                     // change if necessary
-$username = 'superman';                   // set your username
-$password = 'boss';                   // set your password
+require 'phpMQTT.php';
+$server = 'localhost';      // change if necessary
+$port = 1883;              // change if necessary
+$username = 'superman';   // set your username
+$password = 'boss';      // set your password
 $client_id = 'phpMQTT-publisher'; // make sure this is unique for connecting to sever - you could use uniqid()
-$mqtt = new phpMQTT($server, $port, $client_id);
-if ($mqtt->connect(true, NULL, $username, $password)) {
-$mqtt->publish('topic', 'Hello World! at ' . date('r'), 0, false);
-echo "Published message: nv nis ";
-$mqtt->close();
+$topic = 'topic';
+$message = 'Hello World !';
+
+$mqtt = new phpMQTT( $server, $port, $client_id );
+if ($mqtt->connect(true, null, $username, $password)) {
+    $mqtt->publish($topic, $message,0);
+    echo 'Published message: ' . $message ."\n";
+    $mqtt->close();
 } else {
-echo "Time out!\n";
+    echo "Fail or time out !\n";
 }
-
-
-// <!-- <?php
-// require("phpMQTT.php");
-// // require("config.php");
-// $server = 'localhost';     // change if necessary
-// $port = 61616 . "/admin";                     // change if necessary
-// $username = 'admin';                   // set your username
-// $password = 'admin';                   // set your password
-// $client_id = 'phpMQTT-subscriber'; 
-// $message = "Hello CloudAMQP MQTT!";
-// //MQTT client id to use for the device. "" will generate a client id automatically
-// $mqtt = new phpMQTT($server,1883, $client_id);
-// if ($mqtt->connect(true,NULL,$username,$password)) {
-//   $mqtt->publish("topic",$message, 0);
-//   echo "Published message: " . $message;
-//   $mqtt->close();
-// }else{
-//   echo "Fail or time out
-// ";
-// } 
